a6efeb8f5c4873f0a6a03ed59ea5ffa53833ccc3
Neox <129682731+Zufx7r@users.noreply.github.com>
2025-06-19 17:52:16 +0500

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,1 +1,16 @@
-1
+const { Client } = require('@neondatabase/serverless');
+
+exports.handler = async (event) => {
+  const { email, password } = JSON.parse(event.body);
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  await client.connect();
+
+  const res = await client.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+  await client.end();
+
+  if (res.rows.length > 0) {
+    return { statusCode: 200, body: JSON.stringify({ login: true }) };
+  } else {
+    return { statusCode: 401, body: JSON.stringify({ login: false }) };
+  }
+};
