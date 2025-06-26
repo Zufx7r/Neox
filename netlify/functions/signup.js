ceeceaecf349175d15bb6b200a66f946297aecd7
Neox <129682731+Zufx7r@users.noreply.github.com>
2025-06-26 19:42:03 +0500

    Update signup.js

--- netlify/functions/signup.js
+++ netlify/functions/signup.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     },
     body: JSON.stringify({
       email_user: process.env.EMAIL_USER || "❌ EMAIL_USER not set",
-      email_pass: process.env.EMAIL_PASS ? "✅ Exists" : "❌ Missing",
+      email_pass: process.env.EMAIL_PASS ? "✅ Exists" : "❌ Missings",
     }),
   };
 };
